9840a6709403daafed9723a12da202333a8b736f
wesleyornellas <wesleyornellas@outlook.com>
2020-04-24 07:42:58 -0300

    Adicionando projeto

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import Tabela from './Tabela'
+
+function App() {
+
+  const  autores =  [
+    {
+      nome: 'Paulo',
+      livro: 'React',
+      preco: '1000'
+    },
+    {
+      nome: 'Daniel',
+      livro: 'Java',
+      preco: '99'
+    },
+    {
+      nome: 'Marcos',
+      livro: 'Design',
+      preco: '150'
+    },
+    {
+      nome: 'Bruno',
+      livro: 'DevOps',
+      preco: '100'
+    }
+  ];
+
+  return (
+    <div className="App">
+      <Tabela autores = { autores }/>
+    </div>
+  );
+}
+
+export default App;
